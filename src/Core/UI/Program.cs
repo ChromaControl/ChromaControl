c8a617657498e623fa9ba497f04870c0390fa436
Andrew Babbitt <Andrew.Babbitt@outlook.com>
2023-01-14 14:53:45 -0700

    feat: New UI (#213)
    * feat: New website
    
    * Cleanup
    
    * Fix channel string nullability
    
    * Add website pipeline
    
    * Move to ubuntu runners
    
    * feat: New UI
    
    * Enable windows build on linux runners

---
+++ src/Core/UI/Program.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the Chroma Control Contributors under one or more agreements.
+// The Chroma Control Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BlazorDesktop.Hosting;
+using ChromaControl.Core.UI;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = BlazorDesktopHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+if (builder.HostEnvironment.IsDevelopment())
+{
+    builder.UseDeveloperTools();
+}
+
+await builder.Build().RunAsync();
