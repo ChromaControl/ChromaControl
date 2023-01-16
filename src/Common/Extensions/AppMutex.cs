6fdf9953ba18433fdd0ef8d499e5014723ac9e41
Andrew Babbitt <Andrew.Babbitt@outlook.com>
2023-01-16 11:46:29 -0700

    feat: Added common extensions

---
+++ src/Common/Extensions/AppMutex.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the Chroma Control Contributors under one or more agreements.
+// The Chroma Control Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace ChromaControl.Common.Extensions;
+
+/// <summary>
+/// An app mutex.
+/// </summary>
+public class AppMutex
+{
+    /// <summary>
+    /// The app mutex.
+    /// </summary>
+    public Mutex Mutex { get; }
+
+    /// <summary>
+    /// If the mutex creation was successful.
+    /// </summary>
+    public bool Success { get; }
+
+    /// <summary>
+    /// Creates a app mutex.
+    /// </summary>
+    public AppMutex()
+    {
+        Mutex = new Mutex(true, ChromaControlConstants.ExecutingAssemblyName, out var result);
+        Success = result;
+    }
+}
---
+++ src/Common/Extensions/ChromaControlExtensions.cs
@@ -0,0 +1,82 @@
+﻿// Licensed to the Chroma Control Contributors under one or more agreements.
+// The Chroma Control Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using ChromaControl.Common.Keys;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ChromaControl.Common.Extensions;
+
+/// <summary>
+/// Extension methods for Chroma Control.
+/// </summary>
+public static class ChromaControlExtensions
+{
+    /// <summary>
+    /// Allocates a console window.
+    /// </summary>
+    [DllImport("kernel32")]
+    private static extern void AllocConsole();
+
+    /// <summary>
+    /// Adds Chroma Control services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> for adding services.</param>
+    /// <returns>The <see cref="IServiceCollection"/> to allow for method chaining.</returns>
+    /// <param name="args">The command line arguments.</param>
+    public static IServiceCollection AddChromaControl(this IServiceCollection services, string[] args)
+    {
+        if (Debugger.IsAttached || args.Contains("--debug"))
+        {
+            AllocConsole();
+            Console.Title = ChromaControlConstants.ExecutingAssemblyName;
+        }
+
+        if (!Directory.Exists(ChromaControlConstants.DataDirectory))
+        {
+            Directory.CreateDirectory(ChromaControlConstants.DataDirectory);
+        }
+
+        var mutex = new AppMutex();
+
+        if (mutex.Success)
+        {
+            services.AddSingleton(mutex);
+        }
+        else
+        {
+            Environment.Exit(0);
+        }
+
+        if (!Debugger.IsAttached)
+        {
+            AppCenter.Start(KeyData.AppCenterKey, typeof(Analytics), typeof(Crashes));
+        }
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds Chroma Control logging to the specified <see cref="ILoggingBuilder"/>.
+    /// </summary>
+    /// <param name="logging">The <see cref="ILoggingBuilder"/> for adding logging.</param>
+    /// <returns>The <see cref="ILoggingBuilder"/> to allow for method chaining.</returns>
+    public static ILoggingBuilder AddChromaControl(this ILoggingBuilder logging)
+    {
+        if (!Directory.Exists(ChromaControlConstants.LogDirectory))
+        {
+            Directory.CreateDirectory(ChromaControlConstants.LogDirectory);
+        }
+
+        logging.AddFile(Path.Combine(ChromaControlConstants.LogDirectory, $"{ChromaControlConstants.ExecutingAssemblyName}.log"), true);
+
+        return logging;
+    }
+}
