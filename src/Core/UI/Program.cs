932dcf17077b9812ff2a23e4dbb843a6a9d1579a
Andrew Babbitt <Andrew.Babbitt@outlook.com>
2023-01-16 11:17:04 -0700

    chore: Update to BlazorDesktop 3.0

--- src/Core/UI/Program.cs
+++ src/Core/UI/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Components.Web;
 
 var builder = BlazorDesktopHostBuilder.CreateDefault(args);
 
+builder.UseWebViewInstaller();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
