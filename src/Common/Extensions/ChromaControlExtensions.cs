ffc942d95bbb3f575f48fe320df93248443810bd
Andrew Babbitt <andrew.babbitt@outlook.com>
2024-08-07 16:44:49 -0600

    chore: make grpc clients more resiliant

--- src/Common/Extensions/ChromaControlExtensions.cs
+++ src/Common/Extensions/ChromaControlExtensions.cs
@@ -147,7 +147,7 @@ public static class ChromaControlExtensions
 
                         try
                         {
-                            await clientStream.ConnectAsync(10, cancellationToken).ConfigureAwait(false);
+                            await clientStream.ConnectAsync(cancellationToken).ConfigureAwait(false);
                             return clientStream;
                         }
                         catch
